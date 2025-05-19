59a9a757c1b5404f91daeb978b361d07da46cc5a
IrinaAntipina <iramaskaykina@gmail.com>
2025-05-19 10:25:13 +0200

    modularizering

--- fredrik_test_main.py
+++ fredrik_test_main.py
@@ -3,7 +3,7 @@ from taipy.gui import Gui
 from utils.constants import DATA_DIRECTORY
 import pandas as pd
 import os 
-from frontend.del_irina import heat_map
+#from frontend.del_irina import heat_map
 import duckdb
 from colors.color_codes import SALMON_RED, SEA_GREEN
 import plotly.express as px
@@ -125,15 +125,15 @@ with tgb.Page() as page:
                 with tgb.part(class_name="middle-grid"):
                     tgb.text("text text text", class_name="description-text")
                     with tgb.part(class_name="map-card"):
-                        tgb.chart(heat_map)
-                    tgb.part(class_name="map-card")
+                       # tgb.chart(heat_map)
+                        tgb.part(class_name="map-card")
             
             with tgb.part(class_name="right-section"):
                 with tgb.part(class_name="middle-grid"):
                     with tgb.part(class_name="pie-chart-card"):
-                        tgb.chart(heat_map) #examle
-                    with tgb.part(class_name="chart-card"):
-                        tgb.chart(figure="{bar_chart}") # chart for top 10 schools
+                      #  tgb.chart(heat_map) #examle
+                        with tgb.part(class_name="chart-card"):
+                            tgb.chart(figure="{bar_chart}") # chart for top 10 schools
 
 
 
